313bb8f37538477c0c35ce5cb4237590d65f64b3
rafamaxx <rafamaxi.rm@gmail.com>
2021-03-03 02:44:59 -0300

    CommitInicial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie minhaclasse = new Serie();
+            
+        }
+    }
+}
